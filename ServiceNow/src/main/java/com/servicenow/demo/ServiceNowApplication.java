505b9b76e7a072300fd9d63e8c3ad78b7b474296
Vaibhav Shrimali <vaibhav.shrimali@gmail.com>
2017-12-25 22:28:00 +0530

    1ts commit

---
+++ ServiceNow/src/main/java/com/servicenow/demo/ServiceNowApplication.java
@@ -0,0 +1,12 @@
+package com.servicenow.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceNowApplication.class, args);
+	}
+}
